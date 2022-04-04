8413579722da514861e463cc82c6f01c60ddea95
Salih Volkan Özkan <s.volkanozkan@gmail.com>
2022-04-04 22:17:08 +0300

    Part 3 is completed.

--- src/simple/assembly/index.ts
+++ src/simple/assembly/index.ts
@@ -1,8 +1,8 @@
 import { storage, Context } from "near-sdk-as"
 
 // return the string 'hello world'
-export function helloWorld(name: string): string {
-  return 'hello' + name
+export function helloWorld(names: Array<string>): string {
+  return names.map<string>(name => 'hello ' + name).join(` `)
 }
 
 // read the given key from account (contract) storage
